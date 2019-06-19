04f037b7918fc1ab91bc6088da06516c71572986
iyinraphael <iyinraphael@gmail.com>
2019-06-18 21:28:57 -0700

    Tuples

--- Pages/Day-2.xcplaygroundpage/Contents.swift
+++ Pages/Day-2.xcplaygroundpage/Contents.swift
@@ -12,3 +12,9 @@ beatles[1]
 //Set - they are arrange unordered and unique
 let colors = Set(["red", "green", "blue"])
 let colors2 = Set(["red", "green", "blue", "blue"])
+
+//Tuples - you can't add or remive items from a Tuple, there type cannot be change
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.first
+name.last
