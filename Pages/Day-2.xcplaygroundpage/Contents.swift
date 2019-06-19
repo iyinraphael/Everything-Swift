cda03c5ccce3d4fd7f6e130b2d8554cff02511de
iyinraphael <iyinraphael@gmail.com>
2019-06-18 22:01:46 -0700

    Enums associated values

--- Pages/Day-2.xcplaygroundpage/Contents.swift
+++ Pages/Day-2.xcplaygroundpage/Contents.swift
@@ -53,5 +53,14 @@ enum Result {
     case failure
     case success
 }
-
 let result4 = Result.failure
+
+//Enums associated value
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "football")
