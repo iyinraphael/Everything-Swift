b6e870d69e00d0e5f0c5ca83b247c96080ed483c
iyinraphael <iyinraphael@gmail.com>
2019-06-19 22:34:39 -0700

    combininf opeators

--- Pages/Day-3.xcplaygroundpage/Contents.swift
+++ Pages/Day-3.xcplaygroundpage/Contents.swift
@@ -56,3 +56,16 @@ if firstCard + secondCard == 2 {
     print("Regular cards")
 }
 
+//Combining Operators
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Bothe are over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("One of them is over 18")
+}
+
+//Ternary Operator
