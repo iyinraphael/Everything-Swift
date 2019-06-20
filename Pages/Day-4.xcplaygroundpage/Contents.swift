52a9ebafc58985facee5fc20a2dad4e3f9c0581d
iyinraphael <iyinraphael@gmail.com>
2019-06-20 13:32:29 -0700

    for and while loops

--- Pages/Day-4.xcplaygroundpage/Contents.swift
+++ Pages/Day-4.xcplaygroundpage/Contents.swift
@@ -1,3 +1,30 @@
 import Foundation
 
-//Loops
+//For Loops
+let count = 1...10
+for number in count {
+    print("Number is \(number)")
+}
+let albums = ["Red", "1989", "Reputation"]
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+print("Players gonna ")
+for _ in 1...5 {
+    print("play")
+}
+
+
+//While Loops
+var number = 1
+while number <= 20 {
+    print(number)
+    number += 1
+}
+print("Ready or not, here I come!")
+
+var counter = 2
+while counter < 64 {
+    print("\(counter) is a power of 2.")
+    counter *= 2
+}
