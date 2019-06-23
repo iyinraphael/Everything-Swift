b8c9a74448df9ccb9167306973e1d96626f8a14f
iyinraphael <iyinraphael@gmail.com>
2019-06-22 22:55:32 -0700

    closures returning Values and closures as paremeters

--- Pages/Day-6.xcplaygroundpage/Contents.swift
+++ Pages/Day-6.xcplaygroundpage/Contents.swift
@@ -14,3 +14,21 @@ let driving2 = { (place: String) in
 }
 
 driving2("London")
+
+//Returning Valuees
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = drivingWithReturn("Lagos")
+print(message)
+
+//Closures as Parameters
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived")
+}
+
+travel(action: driving)
