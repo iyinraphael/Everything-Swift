b5f97f6255980d749b0c512abd34e3a88f37eab9
iyinraphael <iyinraphael@gmail.com>
2019-06-24 15:23:03 -0700

    shorthand parameter in closure, multiple parameters in closures, return closures in functions

--- Pages/Day-7.xcplaygroundpage/Contents.swift
+++ Pages/Day-7.xcplaygroundpage/Contents.swift
@@ -25,3 +25,33 @@ func travelReturn(action: (String) -> String) {
 travelReturn { (place: String) -> String in
     return "I'm going to \(place) in my private Jet"
 }
+
+//Shorthand Parameter Names
+travelReturn {
+    "I'm going to \($0) in my sport car"
+}
+
+
+//Closures with multiple Parameters
+
+func travelSpeed(action: (String, Int) -> String){
+     print("I'm getting readty to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived")
+}
+
+travelSpeed { "I'm going to \($0) at \($1) miles per hour."
+}
+
+
+//Returning Closures
+
+func travelClose() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let resultCL = travelClose()
+resultCL("London")
