cde7bd575c8cb9e09539c592626ffaef5bf51017
iyinraphael <iyinraphael@gmail.com>
2019-06-25 13:01:33 -0700

    properties and methods of Strings and Arrays

--- Pages/Day-8.xcplaygroundpage/Contents.swift
+++ Pages/Day-8.xcplaygroundpage/Contents.swift
@@ -72,3 +72,20 @@ struct Person {
 var person = Person(name: "Eddy")
 person.makeAnonymous()
 
+
+//Properties and methods of Strings
+
+let string = "Do or do not, there's no try"
+string.count
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+//Properties and methods for Arrays
+
+var toys = ["Woody"]
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
