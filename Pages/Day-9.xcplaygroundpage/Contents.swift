44966f8768fa6b05897c03c83931807053cdd4a5
iyinraphael <iyinraphael@gmail.com>
2019-06-26 12:49:01 -0700

    static properties and methods

--- Pages/Day-9.xcplaygroundpage/Contents.swift
+++ Pages/Day-9.xcplaygroundpage/Contents.swift
@@ -16,9 +16,37 @@ user.username = "iyin"
 
 struct Person2 {
     var name: String
+    lazy var familyTree = FamilyTree()
     
     init(name: String) {
         print("\(name) was born")
         self.name = name
     }
 }
+
+
+//Lazy Properties - Swift will only create the family Tree struc when it's accessed.
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+var ed = Person2(name: "Eddy")
+ed.familyTree
+
+
+// Static properties and Methods
+
+struct Student {
+    static var classSize = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+let taylor = Student(name: "taylor")
+print(Student.classSize)
