815887ca05f5e2426c10432f4ef66aa881588a68
iyinraphael <iyinraphael@gmail.com>
2019-06-27 12:54:38 -0700

    deimitializers and mutibility

--- Pages/Day-10.xcplaygroundpage/Contents.swift
+++ Pages/Day-10.xcplaygroundpage/Contents.swift
@@ -63,3 +63,35 @@ print(singer.name)
 var singerCopy = singer
 singerCopy.name = "Justin Bieber"
 print(singer.name)
+
+
+//Denitializers
+
+class Person {
+    var name = "John Doe"
+    
+    init() {
+        print("\(name) is alive!")
+    }
+    
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit{
+        print("\(name) is no more")
+    }
+
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
+
+
+//Mutability
+
+let taylor = Singer()
+taylor.name = "Ed Sheeran"
+print(taylor.name)
