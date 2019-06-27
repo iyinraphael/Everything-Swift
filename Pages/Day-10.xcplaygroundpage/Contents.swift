53cc9b698d85c5194c9b9ca8d440fb10597f1852
iyinraphael <iyinraphael@gmail.com>
2019-06-27 12:44:27 -0700

    final class and copying objects

--- Pages/Day-10.xcplaygroundpage/Contents.swift
+++ Pages/Day-10.xcplaygroundpage/Contents.swift
@@ -38,3 +38,28 @@ let pop = Poodle(name: "dog")
 pop.makeNoise()
 
 
+//Final class - It can't be subclassed
+
+final class Cat {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+
+//Copying Olbject
+
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+
+var singerCopy = singer
+singerCopy.name = "Justin Bieber"
+print(singer.name)
