ff896f2cee5427fb20d4d9b85eb23aa281d7b935
iyinraphael <iyinraphael@gmail.com>
2019-06-27 12:36:29 -0700

    overriding methods

--- Pages/Day-10.xcplaygroundpage/Contents.swift
+++ Pages/Day-10.xcplaygroundpage/Contents.swift
@@ -11,14 +11,30 @@ class Dog{
         self.name = name
         self.breed = breed
     }
+    
+    func makeNoise() {
+        print("Woof!")
+    }
 }
 
 let poppy = Dog(name: "Poppy", breed: "Poodle")
 
 //Class Inheritance
 
-class poodle: Dog {
+class Poodle: Dog {
     init(name: String) {
         super.init(name: name, breed: "Poodle")
     }
+    
+    override func makeNoise() {
+        print("Yip")
+    }
 }
+
+
+//Overriding Methods
+
+let pop = Poodle(name: "dog")
+pop.makeNoise()
+
+
