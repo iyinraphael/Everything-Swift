31df785ed349847a1917213d22d7230f248c5f99
iyinraphael <iyinraphael@gmail.com>
2019-06-30 15:09:38 -0700

    Protocol Extension, Protocol Oriented Programming

--- Pages/Day-11.xcplaygroundpage/Contents.swift
+++ Pages/Day-11.xcplaygroundpage/Contents.swift
@@ -49,5 +49,23 @@ extension Int {
         return self % 2 == 0
     }
 }
-
 number.isEven
+
+
+
+//Extension of Protocols (POP)
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us: ")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
