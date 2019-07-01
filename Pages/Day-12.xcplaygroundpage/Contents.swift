9456d9a716aea5b8c064c9b851dd7c159f0e410d
iyinraphael <iyinraphael@gmail.com>
2019-07-01 09:44:31 -0700

    Optional try

--- Pages/Day-12.xcplaygroundpage/Contents.swift
+++ Pages/Day-12.xcplaygroundpage/Contents.swift
@@ -57,3 +57,27 @@ let user = username(for: 15) ?? "Anonymous"
 
 let names = ["John", "Paul", "George", "Ringo"]
 let beatle = names.first?.uppercased()
+
+
+
+//Optional try
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+}
+
+if let result = try? checkPassword("password"){
+    print("Result was \(result)")
+} else {
+    print("D'oh.")
+}
+
+try! checkPassword("sekrit")
+print("OK!")
