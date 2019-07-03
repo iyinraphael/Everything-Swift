091bada0ca80862f9a2b15253f2991d9cd7ee2b8
iyinraphael <iyinraphael@gmail.com>
2019-07-03 09:06:26 -0700

    Type of data and swift type inference

--- Pages/Day-13.xcplaygroundpage/Contents.swift
+++ Pages/Day-13.xcplaygroundpage/Contents.swift
@@ -3,3 +3,38 @@ import Foundation
 //Consolidation Day 1 - Variables and Constant
 var name = "Demi Raphael"
 name = "Liz Raphael"
+
+
+
+//Types of Data
+var newName: String
+newName = "Iyin Raphael"
+
+var age: Int
+age = 25
+
+
+
+//Float and Doubles
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+
+
+//Boolean
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false 
