66f6bb9c1158fa764679f24b9b117ea20ef1827a
iyinraphael <iyinraphael@gmail.com>
2019-07-10 21:01:27 -0700

    Polymorphsm

--- Pages/Day-15.xcplaygroundpage/Contents.swift
+++ Pages/Day-15.xcplaygroundpage/Contents.swift
@@ -48,3 +48,64 @@ struct TaylorFan {
 
 let tFan = TaylorFan(name: "James", age: 25)
 print(TaylorFan.favoriteSong) //Because static methods belong to the struct itself rather than to instances of that struct, you can't use it to access any non-static properties from the struct
+
+
+
+
+//Access Control
+
+///Public: this means everyone can read and write the proper
+///Internal: this means only your Swift code can read and write the property. If you ship your code as a framework for others to use, they won’t be able to read the property
+///File Private: this means that only Swift code in the same file as the type can read and write the property.
+///Private: this is the most restrictive option, and means the property is available only inside methods that belong to the type, or its extensions.
+
+
+
+//Polymorphism and typecasting
+
+class Album {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+    
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+    
+    override func getPerformance() -> String {
+        "The studio album \(name) sold lots"
+    }
+}
+
+class LiveAlbum: Album {
+    var location: String
+    
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+    
+    override func getPerformance() -> String {
+        "The live album \(name) sold lots"
+    }
+}
+
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+var allAlbums: [Album] = [taylorSwift, iTunesLive]
+
+for album in allAlbums {
+    print(album.getPerformance())
+}
