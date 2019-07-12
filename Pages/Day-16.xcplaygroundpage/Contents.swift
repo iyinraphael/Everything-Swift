80aa432d984b1316b1fe17b0e6a1bb7d86376346
iyinraphael <iyinraphael@gmail.com>
2019-07-11 19:53:15 -0700

    Protocol Extension

--- Pages/Day-16.xcplaygroundpage/Contents.swift
+++ Pages/Day-16.xcplaygroundpage/Contents.swift
@@ -50,9 +50,45 @@ extension Int {
 
 myInt.plusOne()
 
-
 extension String {
     mutating func trim() {
         self = trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
+
+
+
+//Protocol Extensions
+
+extension Int {
+    func clamp(low: Int, high: Int) -> Int {
+        if self > high {
+            // if we are higher than the upper bound, return the upper bound
+
+            return high
+        } else if self < low {
+            // if we are lower than the lower bound, return the lower bound
+
+            return low
+        }
+        // we are inside the range, so return our value
+
+        return self
+    }
+}
+let i: Int = 8
+print(i.clamp(low: 0, high: 5))
+
+//  So, self means “my current value” and Self means “my current data type.”
+
+extension BinaryInteger {
+    func clamp(low: Self, high: Self) -> Self {
+        if self > high {
+            return high
+        } else if self < low {
+            return low
+        }
+        
+        return self
+    }
+} 
